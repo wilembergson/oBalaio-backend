20cef83ea5dc913da07214c9639b775cbc8782d9
wilembergson <wilembergson@yahoo.com>
2021-06-13 19:12:06 -0300

    Endpoint do frontend

--- src/main/java/com/willembergson/oBalaio/controller/ProdutoController.java
+++ src/main/java/com/willembergson/oBalaio/controller/ProdutoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://obalaio.netlify.app")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
